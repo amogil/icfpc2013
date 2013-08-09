079770d7c52f4d5825736fbc390fa512ac112b8a
Pavel Egorov <xoposhiy@mail.ru>
2013-08-09 23:18:21 +0600

    IF0

--- src/lib/Brute/Force.cs
+++ src/lib/Brute/Force.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using lib.Lang;
 
@@ -16,23 +15,60 @@ namespace lib.Brute
 			{
 				return new Expr[] {new Const(0), new Const(1), new Var("x", v => v.x)};
 			}
-			var topUnary = Solve(sizeMinus1 - 1, ops).SelectMany(ast => GrowTree(ast, ops));
-			var topBinary = SolveTopBinary(sizeMinus1, ops);
-			return topUnary.Concat(topBinary);
+			IEnumerable<Expr> topUnary = Solve(sizeMinus1 - 1, ops).SelectMany(ast => GrowTree(ast, ops));
+			IEnumerable<Expr> topBinary = SolveTopBinary(sizeMinus1, ops);
+			IEnumerable<Expr> topIf = SolveTopIf(sizeMinus1, ops);
+
+			return topUnary.Concat(topBinary).Concat(topIf);
 		}
 
-		private IEnumerable<Expr> SolveTopBinary(int sizeMinus1, string[] ops)
+		private IEnumerable<Expr> SolveTopIf(int resultSize, string[] ops)
 		{
-			var treesWithSize = Enumerable.Range(1, sizeMinus1 - 2)
-				.SelectMany(size => Solve(size, ops).Select(tree => new {size, tree})).ToList();
-			var args = treesWithSize.Join(treesWithSize, g => g.size, g => sizeMinus1 - 1 - g.size, Tuple.Create);
+			if (!ops.Contains("if0")) yield break;
+
+			var treesWithSize = GetTreesWithSizeNotLargerThan(resultSize-3, ops).GroupBy(t => t.Item1, t => t.Item2).ToList();
+			for(int condSize=1; condSize<=resultSize-3; condSize++)
+			{
+				var maxLeftSize = resultSize - 2 - condSize;
+				for (int leftSize = 1; leftSize <= (maxLeftSize+1)/2; leftSize++)
+				{
+					var rightSize = resultSize - 1 - condSize - leftSize;
+					Debug.Assert(leftSize <= rightSize);
+					var condExpr = treesWithSize.FirstOrDefault(g => g.Key == condSize);
+					var leftExpr = treesWithSize.FirstOrDefault(g => g.Key == leftSize);
+					var rightExpr = treesWithSize.FirstOrDefault(g => g.Key == rightSize);
+					if (condExpr == null || leftExpr == null || rightExpr == null) continue;
+					var answers = condExpr.Join(leftExpr, i => 1, o => 1, Tuple.Create).Join(rightExpr, i => 1, o => 1, (tuple, right) => Tuple.Create(tuple.Item1, tuple.Item2, right));
+					foreach (var answer in answers)
+					{
+						yield return new If0(answer.Item1, answer.Item2, answer.Item3);
+						if (leftSize != rightSize)
+							yield return new If0(answer.Item1, answer.Item3, answer.Item2);
+					}
+				}
+			}
+		}
+
+		private IEnumerable<Expr> SolveTopBinary(int resultSize, string[] ops)
+		{
+			List<Tuple<int, Expr>> treesWithSize = GetTreesWithSizeNotLargerThan(resultSize-2, ops);
+			var args = treesWithSize.Join(treesWithSize, g => g.Item1, g => resultSize - 1 - g.Item1, (left, right) => new {left = left.Item2, right = right.Item2});
 			foreach (var op in Binary.BinaryOperators.Where(b => ops.Contains(b.Key)).Select(p => p.Value))
 			{
-				foreach (var ast in args.Select(argsPair => op(argsPair.Item1.tree, argsPair.Item2.tree)))
+				foreach (Expr ast in args.Select(argsPair => op(argsPair.left, argsPair.right)))
 				{
 					yield return ast;
 				}
 			}
+		}
+
+		private List<Tuple<int, Expr>> GetTreesWithSizeNotLargerThan(int maxTreeSize, string[] ops)
+		{
+			if (maxTreeSize < 1) return new List<Tuple<int, Expr>>();
+			List<Tuple<int, Expr>> treesWithSize =
+				Enumerable.Range(1, maxTreeSize)
+				          .SelectMany(size => Solve(size, ops).Select(tree => Tuple.Create(size, tree))).ToList();
+			return treesWithSize;
 		}
 
 		private IEnumerable<Expr> GrowTree(Expr ast, string[] ops)
@@ -44,17 +80,14 @@ namespace lib.Brute
 	[TestFixture]
 	public class Force_Test
 	{
-		[Test]
-		public void Test()
+		[TestCase(5, "shr4 if0", "(shr4 (if0 x x x))")]
+		[TestCase(5, "shr4 if0", "(shr4 (if0 x x x))")]
+		public void Test(int size, string ops, string expectedExpr)
 		{
-			//Size: 8, Operators: and,shl1,shr1
-			var trees = new Force().Solve(7, "and","shl1", "shr1").ToList();
-			
-			Console.WriteLine(trees.Count());
-			foreach (var tree in trees)
-			{
-				Console.WriteLine(tree);
-			}
+			var force = new Force();
+			var trees = force.Solve(size, ops.Split(' '));
+			Assert.IsTrue(trees.Any(tree => tree.ToSExpr() == expectedExpr));
 		}
 	}
+
 }
