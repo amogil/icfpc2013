1f544f007dc1b165266b9a3e84afb86182b93ea3
last_g <last_g@hackerdom.ru>
2013-08-09 23:38:44 +0600

    Fulll test support

---
+++ src/lib/Brute/Full_Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+using lib.Lang;
+using ProblemsMiner;
+
+namespace lib.Brute
+{
+    [TestFixture]
+    internal class Full_Test
+    {
+        [Test]
+        [TestCase(3)]
+        public void TestInclude(int size)
+        {
+            var bruter = new Force();
+            var solutions = new HashSet<Expr>(bruter.Solve(size - 1, new[] {"not", "shl1", "shr1", "shr4", "shr16"}));
+            
+        }
+
+        private IEnumerable<ProblemsMiner.Problem> GetAllSamples(int size)
+        {
+            var files = System.IO.Directory.GetFiles("problems-samples");
+            foreach (var file in files)
+            {
+                var problemsDesriptions = System.IO.File.ReadAllLines(file);
+                foreach (var problem in problemsDesriptions)
+                {
+                    yield return ProblemsMiner.Problem.ParseTraining(problem.Trim());
+                }
+            }
+        }
+
+    }
+}
