a1718bc4560330907c9ddf19c0a9c9a5dadb0f90
Alexey Mogilnikov <alexey@mogilnikov.name>
2013-08-10 01:07:04 +0600

    Тесты

--- src/lib/Guesser/Guesser.cs
+++ src/lib/Guesser/Guesser.cs
@@ -25,17 +25,24 @@ namespace lib.Guesser
     [TestFixture]
     public class Guesser_Test
     {
-        [TestCase("(lambda (x) (and (xor (shr4 x) x) x))", 7, new[] {"and", "shr4", "xor"}, 8)]
-        [TestCase("(lambda (x) (plus (or 1 x) (shl1 x)))", 7, new[] {"or", "plus", "shl1"}, 8)]
-        [TestCase("(lambda (x) (shr4 x))", 3, new[] {"shr4"}, 1)]
-        [TestCase("(lambda (x) (plus x 1))", 4, new[] { "plus" }, 2)]
-        [TestCase("(lambda (x) (not x))", 3, new[] { "not" }, 1)]
+//        [TestCase("(lambda (x) (not x))", 3, new[] {"not"}, 1)]
+//        [TestCase("(lambda (x) (shr4 x))", 3, new[] {"shr4"}, 1)]
+//        [TestCase("(lambda (x) (plus x 1))", 4, new[] {"plus"}, 2)]
+//        [TestCase("(lambda (x) (and (xor (shr4 x) x) x))", 7, new[] {"and", "shr4", "xor"}, 8)]
+//        [TestCase("(lambda (x) (plus (or 1 x) (shl1 x)))", 7, new[] {"or", "plus", "shl1"}, 8)]
+            [TestCase("(lambda (x) (fold x 0 (lambda (x y) (plus (or x y) x))))", 10, new[] {"or", "plus", "fold"}, 8)]
         public void Test(string function, int size, string[] operations, int equalFormulas)
         {
             Expr formula = Expr.ParseFunction(function);
             var random = new Random();
 
             Expr[] trees = new Force().Solve(size - 1, operations).ToArray();
+
+
+            foreach (Expr tree in trees)
+            {
+                Console.WriteLine(tree);
+            }
 
             ulong[] inputs = Enumerable.Range(1, 256).Select(e => random.NextUInt64()).ToArray();
             ulong[] outputs = inputs.Select(i => formula.Eval(new Vars(i))).ToArray();
