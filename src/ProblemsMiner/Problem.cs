fb24e12d36302202cf6a8cd1b91a5e204be897a8
last_g <last_g@hackerdom.ru>
2013-08-09 23:31:42 +0600

    Added parsing of test probems

--- src/ProblemsMiner/Problem.cs
+++ src/ProblemsMiner/Problem.cs
@@ -1,12 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using lib.Lang;
 
 namespace ProblemsMiner
 {
-    internal class Problem
+    public class Problem
     {
         public string Id { get; private set; }
         public int Size { get; private set; }
         public string[] AllOperators { get; private set; }
+        public lib.Lang.Expr Solution { get; private set; }
 
         public string[] FoldOperators
         {
@@ -28,6 +32,51 @@ namespace ProblemsMiner
                     Size = int.Parse(elems.ElementAt(1)),
                     AllOperators = elems.Skip(2).ToArray()
                 };
+        }
+
+        /// <summary>
+        /// Parses problem from training format:
+        ///     kev: value, key1: value
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static Problem ParseTraining(string text)
+        {
+            var problem = new Problem();
+            var keyvalues = text.Split(new[] {", " }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var keyvalue in keyvalues)
+            {
+                var kv = keyvalue.Split(':');
+                switch (kv[0].Trim())
+                {
+                    case "Id":
+                        problem.Id = kv[1].Trim();
+                        break;
+                    case "Size":
+                        problem.Size = int.Parse(kv[1]);
+                        break;
+                    case "Operators":
+                        problem.AllOperators = kv[1].Split(',');
+                        break;
+                    case "Challenge":
+                        problem.Solution = Expr.ParseFunction(kv[1].Trim());
+                        break;
+                }
+            }
+
+            return problem;
+        }
+
+        [Test]
+        public void TestParser()
+        {
+            Assert.AreEqual(new Problem
+                {
+                    Id = "rKHWXidcNEEC6BmTkAeFSeGd",
+                    Size = 6,
+                    AllOperators = new string[] {"shl1","shr1","shr16","shr4"},
+                    Solution = Expr.ParseExpr("(lambda (x_4637) (shr16 (shr1 (shl1 (shr4 x_4637)))))")
+                }, ParseTraining("Id: rKHWXidcNEEC6BmTkAeFSeGd, Challenge: (lambda (x_4637) (shr16 (shr1 (shl1 (shr4 x_4637))))), Size: 6, Operators: shl1,shr1,shr16,shr4"));
         }
     }
 }
