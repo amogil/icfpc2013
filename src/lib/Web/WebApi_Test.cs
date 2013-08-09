62cb44920070ddbc9714f2a12b19690ec77dcba2
Pavel Egorov <xoposhiy@mail.ru>
2013-08-09 17:35:47 +0600

    Add more tests for WebApi

--- src/lib/Web/WebApi_Test.cs
+++ src/lib/Web/WebApi_Test.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Threading;
 using NUnit.Framework;
-using log4net;
-using log4net.Appender;
-using log4net.Config;
-using log4net.Core;
 
 namespace lib.Web
 {
 	[TestFixture]
 	public class WebApi_Test
 	{
-		readonly static WebApi api = new WebApi();
-		private static readonly ILog log = LogManager.GetLogger(typeof (WebApi_Test));
-
 		[TestCase(3)]
 		[TestCase(4)]
 		[TestCase(5)]
@@ -21,10 +13,12 @@ namespace lib.Web
 		[TestCase(8)]
 		[TestCase(9)]
 		[TestCase(10)]
+		[TestCase(10, "tfold")]
+		[TestCase(12, "fold")]
 		[Test]
 		public void GetTrain(int size, params string[] operators)
 		{
-			var trainProblem = api.GetTrainProblem(new TrainRequest(size, operators));
+			TrainProblem trainProblem = api.GetTrainProblem(new TrainRequest(size, operators));
 			Console.WriteLine(trainProblem);
 			Assert.AreEqual(size, trainProblem.size);
 		}
@@ -32,11 +26,13 @@ namespace lib.Web
 		[Test]
 		public void Status()
 		{
-			var status = api.GetStatus();
+			Status status = api.GetStatus();
 			Console.WriteLine("CpuTotalTime: {0}", status.cpuTotalTime);
 			Console.WriteLine("NumReq: {0}", status.numRequests);
 			Console.WriteLine("Req: {0}", status.requestWindow);
 			Console.WriteLine("Cpu: {0}", status.cpuWindow);
 		}
+
+		private static readonly WebApi api = new WebApi();
 	}
 }
