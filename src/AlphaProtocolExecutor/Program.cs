4af28df7bc8324f4691e699cf49f785028386189
Alexey Mogilnikov <alexey@mogilnikov.name>
2013-08-11 02:52:32 +0600

    пРОБЛЕМЫ

--- src/AlphaProtocolExecutor/Program.cs
+++ src/AlphaProtocolExecutor/Program.cs
@@ -9,30 +9,35 @@ namespace AlphaProtocolExecutor
     {
         private static void Main(string[] args)
         {
-//			var problems = File
-//				.ReadAllLines(@"../../../../problems.txt")
-//				.Select(l => l.Trim())
-//				.Where(l => l.Length > 0)
-//				.Select(l => l.Split('\t'))
-//				.Select(elms => new { Id = elms[0], Size = elms[1], Solved = elms[2], Bonus = elms[3], Operations = elms[4].Split(',').Select(t => t.Trim()).ToArray() })
-//				.Select(d => new
-//				{
-//					Id = d.Id,
-//					Size = int.Parse(d.Size),
-//					Tried = d.Solved.Length > 0,
-//					Solved = d.Solved.Equals("True", StringComparison.OrdinalIgnoreCase),
-//					Bonus = d.Bonus.Equals("Bonus", StringComparison.OrdinalIgnoreCase),
-//					Operations = d.Operations,
-//				})
-//				.ToList();
-//			var problemsToSolve = problems.Where(p => !p.Tried && !p.Bonus && p.Size == 11).ToArray();
-//			foreach (var problem in problemsToSolve)
-//			{
-//				AlphaProtocol.PostSolution(problem.Id, problem.Size, problem.Operations, true);
-//			}
+			var problems = File
+				.ReadAllLines(@"../../../../problems.txt")
+				.Select(l => l.Trim())
+				.Where(l => l.Length > 0)
+				.Select(l => l.Split('\t'))
+				.Select(elms => new { Id = elms[0], Size = elms[1], Solved = elms[2], Bonus = elms[3], Operations = elms[4].Split(',').Select(t => t.Trim()).ToArray() })
+				.Select(d => new
+				{
+					Id = d.Id,
+					Size = int.Parse(d.Size),
+					Tried = d.Solved.Length > 0,
+					Solved = d.Solved.Equals("True", StringComparison.OrdinalIgnoreCase),
+					Bonus = d.Bonus.Equals("Bonus", StringComparison.OrdinalIgnoreCase),
+					Operations = d.Operations,
+				})
+				.ToList();
+//            string[] whitelist = new[]
+//                {
+//                  "0rDFjEiGtdYGifXNzi7IwCPU", 
+//                };
+			var problemsToSolve = problems.Where(p => !p.Tried && !p.Bonus && p.Size == 14).ToArray();
+			foreach (var problem in problemsToSolve.Take(5))
+			{
+//                if (whitelist.Any(w => w == problem.Id))
+				    AlphaProtocol.PostSolution(problem.Id, problem.Size, problem.Operations, true);
+			}
 
             // Ручной режим
-            AlphaProtocol.PostSolution("GCDhHD6NjrWTpdg0EAnqthC", 12, "and,fold,not,plus,shl1".Split(','), true);
+//            AlphaProtocol.PostSolution("GCDhHD6NjrWTpdg0EAnqthC", 12, "and,fold,not,plus,shl1".Split(','), true);
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
