4e6cc4e6dae17b6e049ce2f308273c8f68af3a48
Ksenia Zhagorina <ksuhka-zhagorina@yandex.ru>
2013-08-11 19:33:46 +0600

    Генератор входных чисел

---
+++ src/lib/AlphaProtocol/InputGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib.AlphaProtocol
+{
+    public static class InputGenerator
+    {
+        public static UInt64[] Generate()
+        {
+            var samples = new UInt64[256];
+            samples[0] = UInt64.MinValue;
+            samples[1] = UInt64.MaxValue;
+            samples[2] = UInt64.Parse("00000000FFFFFFFF", NumberStyles.AllowHexSpecifier);
+            samples[3] = UInt64.Parse("FFFFFFFF00000000", NumberStyles.AllowHexSpecifier);
+            samples[4] = UInt64.Parse("0F0F0F0F0F0F0F0F", NumberStyles.AllowHexSpecifier);
+            samples[5] = UInt64.Parse("F0F0F0F0F0F0F0F0", NumberStyles.AllowHexSpecifier);
+            samples[6] = UInt64.Parse("5555555555555555", NumberStyles.AllowHexSpecifier);
+            samples[7] = UInt64.Parse("AAAAAAAAAAAAAAAA", NumberStyles.AllowHexSpecifier);
+            var random = new Random(Environment.TickCount);
+            for (int k = 1; k < 32; k++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    UInt64 sample = 0;
+                    var onesCount = 0;
+                    while (onesCount < k)
+                    {
+                        var onePosition = random.Next(64);
+                        if ((sample & (((UInt64)1) << onePosition)) == 0)
+                        {
+                            sample += ((UInt64)1) << onePosition;
+                            onesCount++;
+                        }
+                    }
+                    samples[k * 8 + i * 2] = sample;
+                    samples[k * 8 + i * 2 + 1] = sample ^ UInt64.MaxValue;
+                }
+            }
+            return samples;
+        }
+    }
+}
--- src/lib/AlphaProtocol/SolverTrainer.cs
+++ src/lib/AlphaProtocol/SolverTrainer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using NUnit.Framework;
 using lib.Brute;
@@ -19,14 +20,17 @@ namespace lib.AlphaProtocol
 		{
 			while (true)
 			{
-				var problem = gsc.Train(TrainProblemType.Any, 14);
-				Console.Out.WriteLine("==== TrainProblem: {0}", problem);
+                var problem = gsc.Train(TrainProblemType.Any, 14);
+                Console.Out.WriteLine("==== TrainProblem: {0}", problem);
 
-				var solver = new Solver();
-				var sw = Stopwatch.StartNew();
-				var answer = solver.Solve(problem.id, problem.size, problem.OperatorsExceptBonus);
-				sw.Stop();
-				Console.Out.WriteLine("==== SolvedIn: {0} ms, Answer: {1}", sw.ElapsedMilliseconds, answer);
+                var solver = new Solver();
+                var sw = Stopwatch.StartNew();
+                var answer = solver.Solve(problem.id, problem.size, problem.OperatorsExceptBonus);
+                sw.Stop();
+
+                int solutionSize = Expr.ParseFunction(answer).GetUnified().ToBinExp().ToArray().Size() + 1;
+                int problemSize = Expr.ParseFunction(problem.challenge).GetUnified().ToBinExp().ToArray().Size() + 1;
+                Console.Out.WriteLine("==== SolvedIn: {0} ms, Answer: {1}, ProblemSize: {2}, SolutionSize: {3}", sw.ElapsedMilliseconds, answer, problemSize, solutionSize);
 			}
 		}
 
@@ -100,5 +104,143 @@ namespace lib.AlphaProtocol
 				}
 			}
 		}
+
+
+        [Test]
+        public void CuttingTest()
+        {
+            var keys = GenerateFirstSamples();
+            ProblemsReader reader = ProblemsReader.CreateDefault();
+            for (int i = 8; i < 14; ++i)
+            {
+                var problems =
+                    reader.ReadProblems()
+                          .Where(p => p.Size == i)
+                          .SelectMany(
+                              p => reader.ReadTrainProblems(p, true).Concat(reader.ReadTrainProblems(p, false)).Select(tp => Tuple.Create(p, tp)).Take(2))
+                          .Take(10);
+                foreach (var problemPair in problems)
+                {
+                    var tp = problemPair.Item2;
+                    var programm = Expr.ParseFunction(tp.challenge).GetUnified().ToBinExp().ToArray();
+
+                    var values = keys.ToDictionary(k => k, k => programm.Eval(k));
+                    Func<byte[], bool> check = tree => values.All(kv => tree.Eval(kv.Key) == kv.Value);
+
+                    var solutions = new BinaryBruteForcer(new Mask(values.Values), tp.operators).Enumerate(tp.size - 1);
+                    int count = 0;
+                    int cutted = 0;
+                    foreach (var solution in solutions)
+                    {
+                        ++count;
+                        if (!check(solution))
+                            ++cutted;
+                    }
+                    Console.WriteLine(String.Format("{0}\t{1}\t{2}\t{3}", count, cutted, tp.size, tp));
+                }
+            }
+        }
+
+
+        protected UInt64[] GenerateValuesRANDOM()
+        {
+            Random random = new Random(1234567890);
+            return Enumerable.Range(1, 256).Select(e => random.NextUInt64()).ToArray();
+        }
+
+        protected UInt64[] GenerateValuesAllInMask()
+        {
+            List<UInt64> keys = new List<ulong>();
+            List<int> bits = new List<int>();
+            for (int i = 0; i < 64; ++i)
+                bits.Add(i);
+
+
+
+
+            Random rnd = new Random(1234567890);
+            for (int set = 0; set < 16; ++set)
+            {
+                int[] bitInds = new int[4];
+                for (int i = 0; i < 4; ++i)
+                {
+                    int num = rnd.Next(bits.Count);
+                    bitInds[i] = bits[num];
+                    bits.RemoveAt(num);
+                }
+
+                ulong src = rnd.Next() % 2 == 0 ? 0 : ulong.MaxValue;
+                ulong[] setvalues = new ulong[16];
+                for (int i = 0; i < 16; ++i)
+                    setvalues[i] = src;
+
+
+                for (ulong i = 0; i < 16; ++i)
+                    for (int j = 0; j < 4; ++j)
+                    {
+                        var bitInd = bitInds[j];
+                        var bitValue = i.Bit(j);
+                        if (bitValue)
+                            setvalues[i].SetBit(bitInd);
+                        else
+                            setvalues[i].UnsetBit(bitInd);
+                    }
+                keys.AddRange(setvalues);
+            }
+            
+
+            HashSet<ulong> resultKeys = new HashSet<ulong>();
+            foreach (var k in keys)
+                if (!resultKeys.Contains(k))
+                    resultKeys.Add(k);
+            while (resultKeys.Count < 256)
+            {
+                var k = rnd.NextUInt64();
+                if (!resultKeys.Contains(k))
+                    resultKeys.Add(k);
+            }
+            return resultKeys.ToArray();
+
+        }
+
+        protected UInt64[] GenerateFirstSamples()
+        {
+            var samples = new UInt64[256];
+            samples[0] = UInt64.MinValue;
+            samples[1] = UInt64.MaxValue;
+            samples[2] = UInt64.Parse("00000000FFFFFFFF", NumberStyles.AllowHexSpecifier);
+            samples[3] = UInt64.Parse("FFFFFFFF00000000", NumberStyles.AllowHexSpecifier);
+            samples[4] = UInt64.Parse("0F0F0F0F0F0F0F0F", NumberStyles.AllowHexSpecifier);
+            samples[5] = UInt64.Parse("F0F0F0F0F0F0F0F0", NumberStyles.AllowHexSpecifier);
+            samples[6] = UInt64.Parse("5555555555555555", NumberStyles.AllowHexSpecifier);
+            samples[7] = UInt64.Parse("AAAAAAAAAAAAAAAA", NumberStyles.AllowHexSpecifier);
+            var random = new Random(1234567890);
+            for (int k = 1; k < 32; k++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    UInt64 sample = 0;
+                    var onesCount = 0;
+                    while (onesCount < k)
+                    {
+                        var onePosition = random.Next(64);
+                        if ((sample & (((UInt64)1) << onePosition)) == 0)
+                        {
+                            sample += ((UInt64)1) << onePosition;
+                            onesCount++;
+                        }
+                    }
+                    samples[k*8 + i*2] = sample;
+                    samples[k*8 + i*2 + 1] = sample ^ UInt64.MaxValue;
+                }
+            }
+            return samples;
+        }
+
+	    [Test]
+	    public void TestFirstSamples()
+	    {
+	        var samples = GenerateFirstSamples();
+	    }
 	}
 }
