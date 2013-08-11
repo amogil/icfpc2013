c55c526989a188741c30214e5bd4c3d618253333
andrew <Andrew.Kostousov@gmail.com>
2013-08-11 23:11:22 +0600

    testing

--- src/AlphaProtocolExecutor/SolverProgram.cs
+++ src/AlphaProtocolExecutor/SolverProgram.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using lib.AlphaProtocol;
 using lib.Brute;
 using log4net;
@@ -14,7 +15,7 @@ namespace AlphaProtocolExecutor
 			var gsc = new GameServerClient();
 			while (true)
 			{
-				var problem = gsc.Train(TrainProblemType.Any, 14);
+				var problem = gsc.Train(TrainProblemType.Fold, 16);
 				log.DebugFormat("==== TrainProblem: {0}", problem);
 
 				var solver = new Solver();
@@ -22,6 +23,21 @@ namespace AlphaProtocolExecutor
 				var answer = solver.Solve(problem.id, problem.size, problem.OperatorsExceptBonus, (args, values) => new SmartGenerator(args, values, problem.OperatorsExceptBonus).Enumerate(problem.size - 1));
 				sw.Stop();
 				log.DebugFormat("==== SolvedIn: {0} ms, Answer: {1}", sw.ElapsedMilliseconds, answer);
+			}
+		}
+
+		public static void RunConcurrent()
+		{
+			var gsc = new GameServerClient();
+			while (true)
+			{
+				var problem = gsc.Train(TrainProblemType.Fold, 15);
+				Console.Out.WriteLine("==== TrainProblem: {0}", problem);
+
+				var sw = Stopwatch.StartNew();
+				var answer = ConcurrentWithoutShitAlphaProtocol.PostSolution(problem.id, problem.size, problem.operators);
+				sw.Stop();
+				Console.Out.WriteLine("==== SolvedIn: {0} ms, Answer: {1}", sw.ElapsedMilliseconds, answer);
 			}
 		}
 	}
